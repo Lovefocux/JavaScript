ff5e5bfb16523aa54a2262a91302c662baff0655
Lovefocux <olabodejoshuaf@gmail.com>
2022-06-23 16:57:08 +0100

    zuri Intro to JavaScript

---
+++ IntroJavaScript/script.js
@@ -0,0 +1,17 @@
+// let fname = 'Joshua';
+// let height = 167;
+// let country = 'Nigeria';
+
+// let details = `My name is ${fname}\nMy height is ${height}\nMy Country is ${country}`
+
+// alert(details)
+
+// console.log(details);
+
+let details ={
+   name: 'Joshua',
+   height: '167',
+   country: 'Nigeria',
+}
+
+alert(`My name is ${details.name}, my height is${details.height} tall and my country is ${details.country}.`)
