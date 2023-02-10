ea7c61d6265b86f53cc2abc0edf4b0445f599ee8
Joshua Olabode <olabodejoshuaf@gmail.com>
2023-02-10 15:52:51 +0100

    Variable usage

---
+++ IntroJavaScript/variable.js
@@ -0,0 +1,14 @@
+const variable1 = 10;
+const variable2 = variable1;
+const variable3 = "Joshua";
+const variable4 = {
+   variableType: "object",
+   variableValue: "Some Value",
+};
+const variable5 = (function () {
+   return "Hello, my name is ";
+})();
+
+const variable6 = variable5 + variable3;
+console.log(variable6);
+
